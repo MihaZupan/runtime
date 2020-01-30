2b83d3c2eb45011bd32a0716da502aaa5d8c8e3c
MihaZupan <mihazupan.zupan1@gmail.com>
2020-01-30 07:11:27 +0100

    temp

---
+++ src/libraries/System.Private.Uri/tests/FunctionalTests/KnownSchemeTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace System.PrivateUri.Functional.Tests
+{
+    public static class KnownSchemeTests
+    {
+        [Theory]
+        [InlineData("https")]
+        [InlineData("ws")]
+        [InlineData("wss")]
+        [InlineData("ftp")]
+        [InlineData("file")]
+        [InlineData("gopher")]
+        [InlineData("nntp")]
+        [InlineData("news")]
+        [InlineData("mailto")]
+        [InlineData("uuid")]
+        [InlineData("telnet")]
+        [InlineData("ldap")]
+        [InlineData("net.tcp")]
+        [InlineData("net.pipe")]
+        [InlineData("vsmacros")]
+        public static void KnownSchemes_DoNotAllocateForSchemeLookup(string scheme)
+        {
+            string uriString = scheme + "://foo.bar";
+
+            // Cache this to save on test execution time
+            double allocatedForHttp = s_allocatedForHttp ??= MeasureAllocations(() => new Uri("http://foo.bar"));
+
+            double allocatedForTestScheme = MeasureAllocations(() => new Uri(uriString));
+
+            Assert.InRange(allocatedForTestScheme, allocatedForHttp - 4, allocatedForHttp + 4);
+        }
+        private static double? s_allocatedForHttp = null;
+
+        private static double MeasureAllocations(Action action)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            const long iterations = 10_000;
+
+            long before = GC.GetAllocatedBytesForCurrentThread();
+
+            for (long i = 0; i < iterations; i++)
+                action();
+
+            long after = GC.GetAllocatedBytesForCurrentThread();
+
+            return (after - before) / (double)iterations;
+        }
+    }
+}
